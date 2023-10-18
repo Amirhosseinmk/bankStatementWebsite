9302a9fccc497e58e1d30ce87988f709c2865cfd
Amir <amirhossein.mohammadian@gmail.com>
2023-10-18 17:27:22 -0400

    firstCommit

--- bank.js
+++ bank.js
@@ -1,4 +1,26 @@
-let balance;
-let withdraw;
-let deposit;
-document.getElementById("ENTER").onclick = value;
+
+const Deposit = document.getElementById("increament");
+const Withdraw = document.getElementById("decreament");
+const Enter = document.getElementById("ENTER");
+const Counter = document.getElementById("counter");
+
+function enterB(){
+    
+    var getVal = document.getElementById("option").value;
+    getVal = Number(getVal);
+    var count = Number(Counter.innerHTML);
+    if(Deposit.checked)
+    
+    if(Deposit.checked)
+    {
+
+    count += getVal;
+    }
+    else if(Withdraw.checked)
+    {
+    
+    count -= getVal;
+    }
+    Counter.innerHTML = count;
+
+}
